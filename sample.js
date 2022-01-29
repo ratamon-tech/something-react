eedad1d4fdc5189b6b56d34a607eb1c1fa44ac44
ratamon-tech <ratamon91@gmail.com>
2022-01-29 20:23:27 +0900

    add some

--- sample.js
+++ sample.js
@@ -7,3 +7,21 @@ const LASTNAME = 'Tech'
 const FASTNAME = 'Ratamon'
 
 console.log(`${FASTNAME} ${LASTNAME} is here.`);
+
+const person = (firstName, lastName) => ({
+    first:  firstName,
+    last: lastName
+});
+
+console.log(person("Ratamon", "Tech"));
+
+const tahoe = {
+    mountains: ["Freel", "Rose", "Tallac", "Rubicon", "Silver"],
+    print: function(delay = 1000) {
+        setTimeout(() => {
+            console.log(this.mountains.join(", "));
+        }, delay);
+    }
+};
+
+tahoe.print();
